74f40f8515fa99da9f434da868cad36cf89f743b
Stew Esho <stewardesho@gmail.com>
2019-09-03 13:48:13 -0400

    added bracket rounds data structure to app state

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -10,23 +10,30 @@ class App extends React.Component {
     this.state = {
       bracket: {
         teams: [],
-        matchups: []
+        rounds: []
       },
       lastTeamID: 0,
       isSeeding: true
     };
 
+    this.defaultColors = ['#E91E63', '#FF9800', '#FFEB3B', '#CDDC39', '#4CAF50', '#2196F3', '#9C27B0', '#607D8B'];
+
     this.toggleStage = () => {
       var oldState = this.state;
       oldState.isSeeding = !oldState.isSeeding;
+
+      // Generate bracket when seeding is complete
+      if (!oldState.isSeeding){
+        this.generateBracket();
+      }
       this.setState(oldState);
-    }
+    };
 
     this.updateTeams = (teams) => {
       var stateCopy = this.state;
       stateCopy.bracket.teams = [...teams];
       this.setState(stateCopy);
-    }
+    };
     
     // suite of functions used to edit attributes of a team (to be passed to child components)
     this.editTeam = {
@@ -69,36 +76,72 @@ class App extends React.Component {
         }
         this.updateTeams(teams);
       }
-    }
+    };
 
-    this.defaultColors = ['#E91E63', '#FF9800', '#FFEB3B', '#CDDC39', '#4CAF50', '#2196F3', '#9C27B0', '#607D8B']
-  }
-
-  addTeam = () => {
-    var team = {};
-    team['name'] = team['name'] || '';
-    team['color'] = team['color'] || this.defaultColors[this.state.lastTeamID % this.defaultColors.length];
-    team['logo'] = team['logo'] || imageIcon;
-
-    team.seed = team.seed || this.state.bracket.teams.length + 1;
-    team.id = ++this.state.lastTeamID;
-    let teams = [...this.state.bracket.teams];
-    teams.push(team);
-
-    this.updateTeams(teams);
-    this.setState({ lastTeamID: team.id});
-  }
-
-  removeTeamByID = (id) => {
-    let teams = [...this.state.bracket.teams];
-    let matchingTeam = this.state.bracket.teams.filter(team => team.id === id)[0];
-    if (matchingTeam){
-      let index = this.state.bracket.teams.indexOf(matchingTeam);
-      teams.splice(index, 1);
+    this.addTeam = () => {
+      var team = {};
+      team['name'] = team['name'] || '';
+      team['color'] = team['color'] || this.defaultColors[this.state.lastTeamID % this.defaultColors.length];
+      team['logo'] = team['logo'] || imageIcon;
+  
+      team.seed = team.seed || this.state.bracket.teams.length + 1;
+      team.id = ++this.state.lastTeamID;
+      let teams = [...this.state.bracket.teams];
+      teams.push(team);
+  
       this.updateTeams(teams);
-    }
-  }
+      this.setState({ lastTeamID: team.id});
+    };
 
+    this.removeTeamByID = (id) => {
+      let teams = [...this.state.bracket.teams];
+      let matchingTeam = this.state.bracket.teams.filter(team => team.id === id)[0];
+      if (matchingTeam){
+        let index = this.state.bracket.teams.indexOf(matchingTeam);
+        teams.splice(index, 1);
+        this.updateTeams(teams);
+      }
+    };
+
+    this.generateBracket = () => {
+      var numTeams = this.state.bracket.teams.length;
+      var numRounds = Math.ceil(Math.log2(numTeams));
+      
+      //create rounds array
+      var rounds = [];
+
+      // iterates through rounds backwards (r === 3 <--> first round)
+      // this makes seeding calculations easier
+      for (let r = numRounds; r > 0; r--) {
+        var round = {
+          matches: []
+        };
+
+        for (let m = 0; m < 2 ** (r - 1); m++) {
+          var matchup = { 
+            team1: "",  // Valid values: team seed, "PENDING", "BYE"
+            team2: "",
+            winner: "PENDING"
+          }
+          if (r === numRounds){ //first round
+            matchup.team1 = (m + 1).toString();
+            matchup.team2 = ((2**r) - m) <= numTeams ? ((2**r) - m).toString() : "BYE";
+          } else { //future rounds
+            matchup.team1 = "PENDING";
+            matchup.team2 = "PENDING";
+          }
+          round.matches.push(matchup);
+        }
+
+        rounds.push(round);
+      }
+
+      // Update bracket.
+      var newState = this.state;
+      newState.bracket.rounds = [... rounds];
+      this.setState(newState);
+    };
+  }
 
   render(){
     return (
--- frontend/src/components/seeding/team-entry/TeamEntry.js
+++ frontend/src/components/seeding/team-entry/TeamEntry.js
@@ -24,7 +24,7 @@ class TeamEntry extends React.Component{
                 <div className='right-side'>
                     <label>
                         <input type='file' className='input--logo' accept="image/png, image/jpeg, image/jpg, image/svg+xml" onChange={(e) => this.props.editTeam.editLogo(e, this.props.team.id)} />
-                        <img className='logo--preview' src={this.props.team.logo} />
+                        <img className='logo--preview' src={this.props.team.logo} alt="logo preview" />
                     </label>
                     <div className="button delete" onClick={(e) => this.props.removeTeam(this.props.team.id)}>
                         X
