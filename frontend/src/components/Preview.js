509c44399789ba79454a958e5699943a648259ac
Stew Esho <stewardesho@gmail.com>
2019-09-03 14:38:53 -0400

    basic paper setup???

--- frontend/src/components/Preview.js
+++ frontend/src/components/Preview.js
@@ -1,14 +1,29 @@
 import React from 'react';
+import paper, { Path, Point } from 'paper';
 
 class Preview extends React.Component {
     constructor(props) {
         super(props);
     }
 
+    // Configure paper.js to draw bracket
+    componentDidMount() {
+        paper.setup('canvas');
+
+        // Paper's drawing code
+        var path = new Path();
+        path.strokeColor = 'black';
+        var start = new Point(100, 100);
+        path.moveTo(start);
+        path.lineTo(start + [100, -50]);
+
+        paper.view.draw();
+    }
+
     render() {
         return (
             <div className="Preview">
-                <h1>Preview placeholder</h1>
+                <canvas id="canvas"></canvas>
             </div>
         );
     }
